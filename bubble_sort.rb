91d0c67bf91ae2fd84e41c94c6bde8e2ebf57f3d
Elmer Lopez <meme_lopez10@hotmail.com>
2021-02-19 11:40:50 -0600

    change the method

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,12 +1,32 @@
-def bubble_sort(arr)
-  a = arr.length
-  for x in 0..a do
-    for y in x + 1..a - 1 do
-      if arr[x] > arr[y]
-        arr[x], arr[y] = arr[y], arr[x]
+# def bubble_sort(arr)
+#   a = arr.length
+#   for x in 0..a do
+#     for y in x + 1..a - 1 do
+#       if arr[x] > arr[y]
+#         arr[x], arr[y] = arr[y], arr[x]
+#       end
+#     end
+#   end
+#   print arr
+# end
+# print bubble_sort([4, 3, 78, 2, 0, 2])
+
+
+def bubble_sort(array)
+  array_length = array.size
+  return array if array_length <= 1
+  loop do
+    swapped = false
+    (array_length-1).times do |i|
+      if array[i] > array[i+1]
+        array[i], array[i+1] = array[i+1], array[i]
+        swapped = true
       end
     end
+    break if not swapped
   end
-  print arr
+  array
 end
-print bubble_sort([4, 3, 78, 2, 0, 2])
+
+unsorted_array = [11,5,7,6,15, 100, -1, 0]
+p bubble_sort(unsorted_array)
