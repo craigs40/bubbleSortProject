44771854c999ed523e1565e680a773ea18586204
craigs40 <craig.shay40@gmail.com>
2021-02-19 11:31:12 -0500

    Remove extra end

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -2,10 +2,9 @@ def bubble_sort(arr)
   a = arr.length
   [0..a].each do
     [x + 1..a - 1].each do
-      puts arr[x], arr[y] = arr[y], arr[x] unless arr[x] < arr[y]  
+      puts arr[x], arr[y] = arr[y], arr[x] unless arr[x] < arr[y]
       end
     end
   end
   print arr
-end
 print bubble_sort([4, 3, 78, 2, 0, 2])
