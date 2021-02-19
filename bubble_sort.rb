f20711b9391b6532c54169718fc64f32b1e14721
craigs40 <craig.shay40@gmail.com>
2021-02-19 11:59:57 -0500

    Add bubble_sort_by method

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -7,3 +7,21 @@ def bubble_sort(arr)
   end
 end
 print bubble_sort([4, 3, 78, 2, 0, 2])
+
+def bubble_sort_by(arr)
+  swap = true
+  while swap == true
+    swap = false
+    (arr.length - 1).times do |x|
+    if yield(arr[x], arr[x + 1]) > 0
+      arr[x], arr[x + 1] = arr[x + 1], arr[x]
+      swap = true
+      break if swap == false
+      end
+    end
+  end
+  print arr
+end
+bubble_sort_by(["hi", "hello", "hey"]) do |x, y|
+  x.length - y.length
+end
