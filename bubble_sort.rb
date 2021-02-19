39372325733350044b993196d7b3db9f08b1424a
craigs40 <craig.shay40@gmail.com>
2021-02-19 10:13:07 -0500

    Fix linters

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,12 +1,12 @@
 def bubble_sort(arr)
-    a = arr.length
-    for x in 0..a
-        for y in x + 1..a - 1
-           if arr[x] > arr[y]
-            arr[x], arr[y] = arr[y], arr[x]
-           end
-        end
+  a = arr.length
+  for x in 0..a
+    for y in x + 1..a - 1
+      if arr[x] > arr[y]
+        arr[x], arr[y] = arr[y], arr[x]
+      end
     end
-   print arr
+  end
+  print arr
 end
 print bubble_sort([4,3,78,2,0,2])
